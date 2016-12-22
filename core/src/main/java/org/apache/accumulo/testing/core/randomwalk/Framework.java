ac5b271caec45083751efad7298852969bec387e
Mike Walch <mwalch@apache.org>
2016-12-22 10:39:30 -0500

    ACCUMULO-4510 Adding Randomwalk code from Accumulo
    * All tests are in core module which creates shaded jar
    * Shaded jar can be used on its own or submitted to YARN via Twill

---
+++ core/src/main/java/org/apache/accumulo/testing/core/randomwalk/Framework.java
@@ -0,0 +1,109 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.accumulo.testing.core.randomwalk;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+import com.beust.jcommander.Parameter;
+
+public class Framework {
+
+  private static final Logger log = Logger.getLogger(Framework.class);
+  private HashMap<String,Node> nodes = new HashMap<>();
+  private static final Framework INSTANCE = new Framework();
+
+  /**
+   * @return Singleton instance of Framework
+   */
+  public static Framework getInstance() {
+    return INSTANCE;
+  }
+
+  /**
+   * Run random walk framework
+   *
+   * @param startName
+   *          Full name of starting graph or test
+   */
+  public int run(String startName, State state, Environment env) {
+
+    try {
+      Node node = getNode(startName);
+      node.visit(state, env, new Properties());
+    } catch (Exception e) {
+      log.error("Error during random walk", e);
+      return -1;
+    }
+    return 0;
+  }
+
+  /**
+   * Creates node (if it does not already exist) and inserts into map
+   *
+   * @param id
+   *          Name of node
+   * @return Node specified by id
+   */
+  public Node getNode(String id) throws Exception {
+
+    // check for node in nodes
+    if (nodes.containsKey(id)) {
+      return nodes.get(id);
+    }
+
+    // otherwise create and put in nodes
+    Node node = null;
+    if (id.endsWith(".xml")) {
+      node = new Module(new File("/randomwalk/modules/" + id));
+    } else {
+      node = (Test) Class.forName(id).newInstance();
+    }
+    nodes.put(id, node);
+    return node;
+  }
+
+  static class Opts extends org.apache.accumulo.core.cli.Help {
+    @Parameter(names = "--configDir", required = true, description = "directory containing the test configuration")
+    String configDir;
+    @Parameter(names = "--module", required = true, description = "the name of the module to run")
+    String module;
+  }
+
+  public static void main(String[] args) throws Exception {
+
+    if (args.length != 2) {
+      System.out.println("Usage: Framework <propsPath> <module>");
+      System.exit(-1);
+    }
+
+    Properties props = new Properties();
+    FileInputStream fis = new FileInputStream(args[0]);
+    props.load(fis);
+    fis.close();
+
+    State state = new State();
+    Environment env = new Environment(props);
+    int retval = getInstance().run(args[1], state, env);
+
+    System.exit(retval);
+  }
+}
