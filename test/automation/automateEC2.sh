2e4c9c731bbaaee27d082d06598506e84b48ee07
rcarterjr <russell.c12@gmail.com>
2019-09-17 16:21:23 -0400

    read from args file

--- test/automation/automateEC2.sh
+++ test/automation/automateEC2.sh
@@ -16,31 +16,56 @@
 # limitations under the License.
 
 
-ACCUMULO_REPO=$1
-ACCUMULO_TESTING_REPO=$2
-MUCHOS_REPO=$3
-
 while true; do
 
-	# builds Accumulo tarball in a temporary directory
-	mkdir ~/accumulotmp && cd ~/accumulotmp
-	git clone --single-branch --branch 2.0 --depth=1 $ACCUMULO_REPO && cd accumulo
+	# pulls arguments from the text file
+	ARGSFILE=$1
+	I=0
+	while IFS= read -r line; do
+		ARG[$I]="$line";
+		I=$((I+1));
+	done <"$ARGSFILE"
+
+
+	if [ $I != 7 ]; then
+		echo 'Missing arguments in args.txt. Check README.md'
+		break;
+	fi
+
+	ACCUMULO_REPO=${ARG[0]}
+	ACCUMULO_BR=${ARG[1]}
+
+	ACCUMULO_TESTING_REPO=${ARG[2]}
+	ACCUMULO_TESTING_BR=${ARG[3]}
+
+	MUCHOS_REPO=${ARG[4]}
+	MUCHOS_BR=${ARG[5]}
+	MUCHOS_CONFIG=${ARG[6]} # overwrite muchos.props in cloned repository
+
+
+
+	# builds Accumulo tarball and installs fluo-muchos in a temporary directory
+	TMPDIR=`mktemp -d`
+	echo "Directory created: $TMPDIR " && cd $TMPDIR
+	git clone --single-branch --branch $MUCHOS_BR $MUCHOS_REPO 
+	cd fluo-muchos && cd ..
+	git clone --single-branch --branch $ACCUMULO_BR $ACCUMULO_REPO && cd accumulo
 	mvn clean package -DskipFormat -PskipQA
-	
+
 	# copies the tarball to the given muchos directory
-	cp ./assemble/target/*.gz $MUCHOS_REPO/conf/upload/
+	cp ./assemble/target/*.gz ~/fluo-muchos/conf/upload/
 	if [ $? -eq 0 ]; then
 		echo "Accumulo tarball copied to fluo-muchos"
-		rm -rf ~/accumulotmp
 	else
 		break
-		rm -rf ~/accumulotmp
 	fi
 
 	# sets up the cluster
-	cd $MUCHOS_REPO || (echo "Incorrect fluo-muchos path: $MUCHOS_REPO" && break)
+	cd $TMPDIR/fluo-muchos || (echo "Could not find Fluo-Muchos" && break)
+	cp conf/muchos.props.example conf/muchos.props
+	cp $MUCHOS_CONFIG ./conf/muchos.props || (echo Could not use custom config. Check paths in args.txt)
+
 	./bin/muchos launch -c "$USER-cluster" && echo "Setting up cluster.."
-	
 	# repeat setup until all nodes are intialized
 	until ./bin/muchos setup; do
 		echo "Script will resume once nodes are intialized."
@@ -61,16 +86,10 @@ while true; do
 	PROXYIP=`./bin/muchos config -p proxy.public.ip`
 	M2='/home/centos/install/apache-maven*/bin'
 	
-	# installs java-11 and makes it the default for mvn
-	ssh $CLUSTERUSER@$PROXYIP "sudo yum -y install java-11-openjdk-devel &"
-	ssh $CLUSTERUSER@$PROXYIP "sed -i '/export JAVA_HOME/d' ~/.bashrc"
-	ssh $CLUSTERUSER@$PROXYIP "sed -i '/# .bashrc/a export JAVA_HOME=/usr/lib/jvm/java-11-openjdk' ~/.bashrc"
-	ssh $CLUSTERUSER@$PROXYIP "source ~/.bashrc"
-
 	# clones and builds accumulo and  accumulo-testing to EC2
-	ssh $CLUSTERUSER@$PROXYIP "git clone --single-branch --branch 2.0 --depth=1 $ACCUMULO_REPO &"
+	ssh $CLUSTERUSER@$PROXYIP "git clone --single-branch --branch $ACCUMULO_BR $ACCUMULO_REPO &"
 	ssh $CLUSTERUSER@$PROXYIP "cd accumulo && $M2/mvn clean install -PskipQA && cd .. &"
-	ssh $CLUSTERUSER@$PROXYIP "git clone $ACCUMULO_TESTING_REPO &"
+	ssh $CLUSTERUSER@$PROXYIP "git clone --single-branch --branch $ACCUMULO_TESTING_BR $ACCUMULO_TESTING_REPO &"
 	ssh $CLUSTERUSER@$PROXYIP "cd accumulo-testing && $M2/mvn clean package &"
 
 	break
