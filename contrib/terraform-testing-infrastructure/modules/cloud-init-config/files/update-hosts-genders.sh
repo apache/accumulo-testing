63078cf483232d955b25b0d21f9a06636d9bcf3f
Brian Loss <brianloss@gmail.com>
2022-02-08 11:47:37 -0500

    Initialize Yarn to allow ci tests to run
    * Add systemd unit files for resourcemanager and nodemanagers that are
      enabled and started after HDFS.
    * Add a minimal yarn-site.xml configuration that enables MapReduce and
      names the resource manager.
    * Update the bashrc file to include maven in the path
    * Modify the accumulo-testing repo's conf file to name the
      resourcemanager host and HDFS default.
    * Modify initial cluster setup script to move hosts and genders marker
      lines from the template into the update script to consolidate logic.

--- contrib/terraform-testing-infrastructure/modules/cloud-init-config/files/update-hosts-genders.sh
+++ contrib/terraform-testing-infrastructure/modules/cloud-init-config/files/update-hosts-genders.sh
@@ -10,22 +10,43 @@ fi
 HOSTS_ADDITIONS=$1
 GENDERS_ADDITIONS=$2
 
+begin_hosts_marker="##### BEGIN GENERATED HOSTS #####"
+end_hosts_marker="##### END GENERATED HOSTS #####"
+begin_genders_marker="##### BEGIN GENERATED GENDERS #####"
+end_genders_marker="##### END GENERATED GENDERS #####"
+
 # Update the hosts file locally
-sudo sed -ri '/^#+ BEGIN GENERATED HOSTS #+$/,/^#+ END GENERATED HOSTS #+$/d' /etc/hosts
-cat "$HOSTS_ADDITIONS" | sudo tee -a /etc/hosts > /dev/null
+# Wrap the supplied host additions with markers that we'll use to strip it back out.
+TMPHOSTS=/tmp/hosts$$
+cat > $TMPHOSTS <<EOF
+$begin_hosts_marker
+##### DO NOT EDIT THIS SECTION #####
+$(<"$HOSTS_ADDITIONS")
+$end_hosts_marker
+EOF
+# Strip out any previously applied hosts additiona, and then tack the new ones on to the end of /etc/hosts.
+sudo sed -ri '/^'"$begin_hosts_marker"'$/,/^'"$end_hosts_marker"'$/d' /etc/hosts
+cat "$TMPHOSTS" | sudo tee -a /etc/hosts > /dev/null
 
 # Update the genders file locally
-[[ -f /etc/genders ]] && sudo sed -ri '/^#+ BEGIN GENERATED GENDERS #+$/,/^#+ END GENERATED GENDERS #+$/d' /etc/genders
-cat "$GENDERS_ADDITIONS" | sudo tee -a /etc/genders > /dev/null
+TMPGENDERS=/tmp/genders$$
+cat > $TMPGENDERS <<EOF
+$begin_genders_marker
+$(<"$GENDERS_ADDITIONS")
+$end_genders_marker
+EOF
+[[ -f /etc/genders ]] && sudo sed -ri '/^'"$begin_genders_marker"'$/,/^'"$end_genders_marker"'$/d' /etc/genders
+cat "$TMPGENDERS" | sudo tee -a /etc/genders > /dev/null
 echo "Check genders file validity..."
 nodeattr -k
 
 # Now copy hosts updates to the workers and apply
-TMPFILE=/tmp/hosts$$
-pdcp -g worker "$HOSTS_ADDITIONS" $TMPFILE
-pdsh -S -g worker 'sudo sed -ri '"'"'/^#+ BEGIN GENERATED HOSTS #+$/,/^#+ END GENERATED HOSTS #+$/d'"'"' /etc/hosts'
-pdsh -S -g worker 'cat '$TMPFILE' | sudo tee -a /etc/hosts > /dev/null && rm -f $TMPFILE'
+pdcp -g worker $TMPHOSTS $TMPHOSTS
+pdsh -S -g worker 'sudo sed -ri '"'"'/^'"$begin_hosts_marker"'$/,/^'"$end_hosts_marker"'$/d'"'"' /etc/hosts'
+pdsh -S -g worker 'cat '$TMPHOSTS' | sudo tee -a /etc/hosts > /dev/null && rm -f $TMPHOSTS'
+rm -f $TMPHOSTS
 
 # Copy genders updates to the workers and apply
-pdcp -g worker /etc/genders /tmp/genders
-pdsh -S -g worker "sudo cp /tmp/genders /etc/genders && rm /tmp/genders"
+pdcp -g worker $TMPGENDERS $TMPGENDERS
+pdsh -S -g worker "sudo cp $TMPGENDERS /etc/genders && rm -f $TMPGENDERS"
+rm -f $TMPGENDERS
