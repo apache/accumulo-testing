a13ec5f74b1afefe85fb54fba449ecbed5d0cc3b
DomGarguilo <dominic.garguilo@gmail.com>
2023-06-28 13:01:30 -0400

    Bump maven to 3.8.8 in terraform config

--- contrib/terraform-testing-infrastructure/azure/variables.tf
+++ contrib/terraform-testing-infrastructure/azure/variables.tf
@@ -209,7 +209,7 @@ variable "accumulo_dir" {
 }
 
 variable "maven_version" {
-  default     = "3.8.6"
+  default     = "3.8.8"
   description = "The version of Maven to download and install"
   nullable    = false
 }
