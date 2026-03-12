293979f961781070276f8dc5e7d3e853ccc1d04a
Keith Turner <kturner@apache.org>
2026-03-12 15:45:11 -0400

    changes to support building with java 21 (#305)

--- src/main/java/org/apache/accumulo/testing/stress/DataWriter.java
+++ src/main/java/org/apache/accumulo/testing/stress/DataWriter.java
@@ -18,27 +18,19 @@
  */
 package org.apache.accumulo.testing.stress;
 
-import java.lang.ref.Cleaner.Cleanable;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.accumulo.core.client.BatchWriter;
 import org.apache.accumulo.core.client.MutationsRejectedException;
-import org.apache.accumulo.core.util.cleaner.CleanerUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class DataWriter extends Stream<Void> implements AutoCloseable {
   private final BatchWriter writer;
   private final RandomMutations mutations;
-  private final Cleanable cleanable;
   private final AtomicBoolean closed = new AtomicBoolean(false);
-
-  private static final Logger log = LoggerFactory.getLogger(DataWriter.class);
 
   public DataWriter(BatchWriter writer, RandomMutations mutations) {
     this.writer = writer;
     this.mutations = mutations;
-    this.cleanable = CleanerUtil.unclosed(this, DataWriter.class, closed, log, writer);
   }
 
   @Override
@@ -54,9 +46,6 @@ public class DataWriter extends Stream<Void> implements AutoCloseable {
   @Override
   public void close() {
     if (closed.compareAndSet(false, true)) {
-      // deregister cleanable, but it won't run because it checks
-      // the value of closed first, which is now true
-      cleanable.clean();
       try {
         writer.close();
       } catch (MutationsRejectedException e) {
